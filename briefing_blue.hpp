322f61adeb1417b2d7ec618d7a21419c6da89c92
Ballista Milsim <ballista.milsim@gmail.com>
2017-04-17 16:36:31 +0200

    tb_rhs_ascent.mountains_acr mission added

---
+++ briefing_blue.hpp
@@ -0,0 +1,20 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base with vehicles at AT Emplacment<br />
+<boxes> Forward Base with vehicles at Mountain Pass<br />
+<boxes> Forward Base at Camp Xray<br />
+<boxes> Forward Base at Outpost<br />
+<RP> Team Rally Point at Roadblock<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> The majority of the team shoud defend the first objective<br />
+<*> The rest should defend AT Emplacement<br />
+<br />
+
+<titleFont>Tips:</titleFont><br />
+<*> <yellow>Successfully defending AT Emplacement denies the enemy assets and gives you a powerful fire base late in the game</yellow>
---
+++ briefing_red.hpp
@@ -0,0 +1,19 @@
+// Provide side specific mission briefing here. Text can use structured text syntax. Use <br /> tag for line breaks, plus all other Arma tags.
+<titleFont>Gerneral Reminders:</titleFont><br />
+<*> <green>Squad leaders</green> - take the SL kit and deploy <RP> Rally Points<br />
+<*> Help out your team by deploying <FO> Forward Outposts<br />
+<br />
+
+<titleFont>Deployment Options:</titleFont><br />
+<boxes> Forward Base with vehicles unlocked at Refuge on capture of Roadblock<br />
+<RP> Team Rally Point east of Refuge<br />
+<RP> Team Rally Point north-west of AT Emplacement<br />
+<br />
+
+<titleFont>Strategies:</titleFont><br />
+<*> The majority of the team shoud attack the first objective<br />
+<*> The rest should attack AT Emplacement and neutralise enemy marksman in the surrounding hills<br />
+<br />
+
+<titleFont>Tips:</titleFont><br />
+<*> <yellow>Successfully attacking AT Emplacement give you access to CAS helicopters and heavy armour at Main Base</yellow>
